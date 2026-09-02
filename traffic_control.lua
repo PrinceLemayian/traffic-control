9d1dee7d7c5c3da91489737922d9cb5e5387ffb3
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-09-02 23:52:39 +0300

    feat: implement road coroutine with persistent queue and wait state

--- traffic_control.lua
+++ traffic_control.lua
@@ -12,3 +12,27 @@ local CONGESTION_W = 1.0
 local FAIRNESS_W = 2.5
 local STARVATION_LIMIT = 3
 local SIM_CYCLES = 20
+
+local function make_road(def)
+	return coroutine.create(function(got_green)
+		local queue = def.queue
+		local waiting = 0
+
+		while true do
+			if got_green then
+				queue = math.max(0, queue - def.flow)
+				waiting = 0
+			else
+				waiting = waiting + 1
+			end
+
+			queue = queue + math.random(0, def.arrival * 2)
+
+			got_green = coroutine.yield({
+				name = def.name,
+				queue = queue,
+				waiting = waiting,
+			})
+		end
+	end)
+end
